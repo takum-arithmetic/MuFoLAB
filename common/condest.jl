e0d79287d2f2e064ef030bcf335ba57629bd4f60
jamesquinlan <james.quinlan@maine.edu>
2024-07-29 08:44:16 -0400

    Estimate condition of A

---
+++ common/condest.jl
@@ -0,0 +1,22 @@
+""" 
+    condest(A::AbstractMatrix{T}) where {T <: AbstractFloat} -> Float64 
+
+    Estimates the condition number of a matrix `A` using the p-norm.
+    condest(A) = ||A||_p * ||A^{-1}||_p.  
+    Method based on [1, p.372].  Note: [1] uses the 1-norm.
+
+Reference 
+----------
+[1] Cline, A. K., Moler, C. B., Stewart, G. W., & Wilkinson, J. H. (1979). 
+An estimate for the condition number of a matrix. SIAM Journal on 
+Numerical Analysis, 16(2), 368-375.
+
+
+"""
+function condest(A::AbstractMatrix{T}; p = Inf) where {T <: AbstractFloat}
+        n = size(A, 1)
+        b = T.(rand([1, -1], n))
+        x = A' \ b
+        y = A \ x
+        return norm(A, p) * (norm(y, p) / norm(x, p))
+end
