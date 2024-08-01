20916b52b487df910d8b660de50e0373c5973e91
Laslo Hunhold <hunhold@uni-koeln.de>
2024-08-01 17:54:33 +0200

    Further refactor utilities to condense usage in each experiment
    Every experiment now simply contains the solver function and an
    optional preconditioner. The output file name will be detected
    automatically.
    
    Signed-off-by: Laslo Hunhold <hunhold@uni-koeln.de>

--- src/Utilities.jl
+++ src/Utilities.jl
@@ -2,11 +2,14 @@
 module Utilities
 
 using Base.Threads
+using BFloat16s
 using CSV
 using DataFrames
 using LinearAlgebra
+using SoftPosit
 using SparseArrays
 using Suppressor
+using Takums
 
 @suppress_err begin
 	using MatrixDepot
@@ -27,6 +30,10 @@ number_types = [
 	Float64,
 ]
 
+struct SolveData
+	absolute_error::Float64
+end
+
 function sparsecast(t::Type{T}, M::AbstractMatrix) where {T <: AbstractFloat}
 	if typeof(M) <: SparseMatrixCSC
 		return SparseMatrixCSC{t, Int64}(M)
@@ -37,7 +44,12 @@ function sparsecast(t::Type{T}, M::AbstractMatrix) where {T <: AbstractFloat}
 	end
 end
 
-function run_experiment(f::Function, types::Vector{DataType}, matrix_indices::Vector{Int64})
+function run_experiment(
+	f::Function,
+	types::Vector{DataType},
+	matrix_indices::Vector{Int64},
+	parameters::Any = nothing,
+)
 	# get return type of passed function and generate output matrix
 	f_return_types = Base.return_types(f)
 	if (length(f_return_types) != 1)
@@ -48,7 +60,7 @@ function run_experiment(f::Function, types::Vector{DataType}, matrix_indices::Ve
 	@threads for i in 1:length(matrix_indices)
 		M = matrixdepot(sp(matrix_indices[i]))
 		@threads for j in 1:length(types)
-			results[j, i] = f(M, types[j])
+			results[j, i] = f(M, types[j], parameters)
 		end
 	end
 
@@ -92,4 +104,70 @@ function write_experiment_csv(
 	# print the written file name to standard output for the witness file
 	return println(file_name)
 end
+
+# the solver experiments compare the solutions of linear systems of equations
+# Ax = b via the infinity norm.
+struct SolverParameters
+	solver::Function
+	preconditioner::Union{Nothing, Function}
+end
+
+function solver_experiment_callback(
+	M::AbstractMatrix,
+	t::Type{T},
+	parameters::SolverParameters,
+) where {T <: AbstractFloat}
+	local x
+
+	A = Utilities.sparsecast(t, M)
+	y = ones(t, size(A, 1))
+	b = A * y
+
+	try
+		if parameters.preconditioner != nothing
+			A, b = parameters.preconditioner(A, b)
+		end
+		x = parameters.solver(A, b)
+	catch
+		x = fill(NaN, size(A, 1))
+	end
+
+	absolute_error = norm(Float64.(y) - Float64.(x), Inf)
+
+	return SolveData(absolute_error)
+end
+
+function run_solver_experiment(; solver::Function, preconditioner::Union{Function, Nothing})
+	matrix_indices = sort(
+		getfield.(
+			listdata(
+				@pred(
+					10^1 <=
+					n <=
+					2 * 10^2 &&
+					n == m &&
+					nnz / n > 10
+				)
+			),
+			:id,
+		),
+	)
+
+	results = Utilities.run_experiment(
+		solver_experiment_callback,
+		Utilities.number_types,
+		matrix_indices,
+		SolverParameters(solver, preconditioner),
+	)
+
+	experiment_name = chopsuffix(basename(PROGRAM_FILE), ".jl")
+
+	return Utilities.write_experiment_csv(
+		"out/" * experiment_name * "-absolute_error.csv",
+		number_types,
+		matrix_indices,
+		getfield.(results, :absolute_error),
+	)
+end
+
 end
--- src/solve_direct.jl
+++ src/solve_direct.jl
@@ -1,59 +1,9 @@
 # See LICENSE file for copyright and license details.
-using BFloat16s
-using LinearAlgebra
-using SoftPosit
-using SparseArrays
-using Suppressor
-using Takums
-
-@suppress_err begin
-	using MatrixDepot
-end
-
 include("Utilities.jl")
 using .Utilities
 
-"""
-    Experiment 1: Compare solutions for different types of numbers for 
-    solving linear systems of equations, Ax = b. In this experiment, 
-    we compare the infinity norm of the difference between the true 
-    solution and the computed solution for different types of numbers. 
-    The types of numbers we compare are Takums, SoftPosits, BFloat16s, and 
-    Floats.  We use the SuiteSparse Matrix Collection to test the different 
-    number types.  The integer ids are the 'official' ident numbers 
-    assigned by the collection. Currently id ∈ 1:3000.
-"""
-struct SolveData
-	absolute_error::Float64
+function solve_direct(A::AbstractMatrix, b::AbstractVector)
+	return A \ b
 end
 
-function solve_direct(M::AbstractMatrix, t::Type{T}) where {T <: AbstractFloat}
-	local x
-
-	A = Utilities.sparsecast(t, M)
-	y = ones(t, size(A, 1))
-	b = A * y
-
-	try
-		x = A \ b
-	catch
-		x = fill(NaN, size(A, 1))
-	end
-
-	absolute_error = norm(Float64.(y) - Float64.(x), Inf)
-
-	return SolveData(absolute_error)
-end
-
-matrix_indices = sort(
-	getfield.(listdata(@pred(10^1 <= n <= 2 * 10^2 && n == m && nnz / n > 10)), :id),
-)
-
-results = Utilities.run_experiment(solve_direct, Utilities.number_types, matrix_indices)
-
-Utilities.write_experiment_csv(
-	"out/solve_direct.csv",
-	types,
-	matrix_indices,
-	getfield.(results, :absolute_error),
-)
+Utilities.run_solver_experiment(; solver = solve_direct, preconditioner = nothing)
