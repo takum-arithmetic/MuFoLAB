2b3ecdd24cc9128875c0f5b46fe24259b5af221d
Laslo Hunhold <hunhold@uni-koeln.de>
2024-08-12 23:50:49 +0200

    Make sure the orthogonal matrix is of the desired type
    Signed-off-by: Laslo Hunhold <hunhold@uni-koeln.de>

--- src/solve_qr.jl
+++ src/solve_qr.jl
@@ -2,13 +2,14 @@
 using SparseArrays
 
 push!(LOAD_PATH, "src/")
+using Crutches
 using Experiments
 import QR
 import TestMatrices
 
 function solve_qr(A::AbstractMatrix, b::AbstractVector)
 	Q, R = QR.qr_givens(A)
-	Q_full = Q * spdiagm(ones(size(A, 1)))
+	Q_full = Q * spdiagm(ones(typeof(A[1,1]), (size(A, 1))))
 	z = Q_full' * b
 	return R \ z
 end
@@ -16,7 +17,6 @@ end
 # TODO
 test_matrices = TestMatrices.get_test_matrices(:sparse)
 filter!(t -> (t.m == t.n && t.rank == t.m && t.nnz in 200:1000), test_matrices)
-test_matrices = test_matrices[1:1]
 
 write_experiment_results(
 	ExperimentResults(
