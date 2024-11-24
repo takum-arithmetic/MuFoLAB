42a500e1d698eb93c75810462a4e9e2294af46b2
Laslo Hunhold <hunhold@uni-koeln.de>
2024-11-24 19:34:13 +0100

    Add Float8 crutches and remove SoftPosit crutches
    The latter are not necessary anymore as we switched to Posits.jl.
    
    Signed-off-by: Laslo Hunhold <hunhold@uni-koeln.de>

--- src/Crutches.jl
+++ src/Crutches.jl
@@ -2,35 +2,106 @@
 module Crutches
 
 using BFloat16s
-using SoftPosit
-
-# rounding and truncation are incomplete in SoftPosit
-Base.round(p::AbstractPosit, r::RoundingMode{:ToZero})  = typeof(p)(Base.trunc(Float64(p)))
-Base.round(p::AbstractPosit, r::RoundingMode{:Down})    = typeof(p)(Base.floor(Float64(p)))
-Base.round(p::AbstractPosit, r::RoundingMode{:Up})      = typeof(p)(Base.ceil(Float64(p)))
-Base.round(p::AbstractPosit, r::RoundingMode{:Nearest}) = typeof(p)(Base.round(Float64(p)))
-
-Base.trunc(p::AbstractPosit) = Base.signbit(p) ? Base.ceil(p) : Base.floor(p)
-Base.trunc(::Type{T}, p::AbstractPosit) where {T <: Integer} = Base.trunc(T, Float64(p))
-
-# integer powers are broken for negative exponents, thus we hijack
-# the original function and, if the exponent is negative, negate it and
-# invert the result.
-function Base.:(^)(x::AbstractPosit, y::Integer)
-	return if sign(y) < 0
-		inv(invoke(Base.:(^), Tuple{Real, Integer}, x, -y))
-	else
-		invoke(Base.:(^), Tuple{Real, Integer}, x, y)
-	end
-end
-
-# also fix posit type promotion
-Base.promote_rule(::Type{Float16}, ::Type{<:AbstractPosit}) = Float16
-Base.promote_rule(::Type{Float32}, ::Type{<:AbstractPosit}) = Float32
-Base.promote_rule(::Type{Float64}, ::Type{<:AbstractPosit}) = Float64
+using Float8s
+using LinearAlgebra
 
 # BFloat16s defines truncation to Int, whereas it should be more generally
 # defined to be Integer
 Base.trunc(::Type{Integer}, f::BFloat16) = Base.trunc(Int, f)
+
+# Float8s does not define eps()
+Base.eps(::Type{Float8}) = Float8(2^-4)
+
+# Integer truncation is incomplete in Float8s
+Base.trunc(::Type{T}, f::Float8) where {T <: Integer} = Base.trunc(T, Float64(f))
+
+# givens_algorithm is broken in Float8s given its small dynamic range,
+# we add the second count >= 20 check while pursuing an upstream fix.
+#
+# The function is licensed as follows:
+#
+# Copyright (c) 2009-2024: Jeff Bezanson, Stefan Karpinski, Viral B. Shah,
+# and other contributors: https://github.com/JuliaLang/julia/contributors
+#
+# Permission is hereby granted, free of charge, to any person obtaining a
+# copy of this software and associated documentation files (the "Software"),
+# to deal in the Software without restriction, including without limitation
+# the rights to use, copy, modify, merge, publish, distribute, sublicense,
+# and/or sell copies of the Software, and to permit persons to whom the
+# Software is furnished to do so, subject to the following conditions:
+#
+# The above copyright notice and this permission notice shall be included
+# in all copies or substantial portions of the Software.
+function LinearAlgebra.givensAlgorithm(f::Float8, g::Float8)
+	T = Float8
+	onepar = one(T)
+	T0 = typeof(onepar) # dimensionless
+	zeropar = T0(zero(T)) # must be dimensionless
+
+	# need both dimensionful and dimensionless versions of these:
+	safmn2 = LinearAlgebra.floatmin2(T0)
+	safmn2u = LinearAlgebra.floatmin2(T)
+	safmx2 = one(T) / safmn2
+	safmx2u = oneunit(T) / safmn2
+
+	if g == 0
+		cs = onepar
+		sn = zeropar
+		r = f
+	elseif f == 0
+		cs = zeropar
+		sn = onepar
+		r = g
+	else
+		f1 = f
+		g1 = g
+		scalepar = max(abs(f1), abs(g1))
+		if scalepar >= safmx2u
+			count = 0
+			while true
+				count += 1
+				f1 *= safmn2
+				g1 *= safmn2
+				scalepar = max(abs(f1), abs(g1))
+				if scalepar < safmx2u || count >= 20
+					break
+				end
+			end
+			r = sqrt(f1 * f1 + g1 * g1)
+			cs = f1 / r
+			sn = g1 / r
+			for i in 1:count
+				r *= safmx2
+			end
+		elseif scalepar <= safmn2u
+			count = 0
+			while true
+				count += 1
+				f1 *= safmx2
+				g1 *= safmx2
+				scalepar = max(abs(f1), abs(g1))
+				if scalepar > safmn2u || count >= 20
+					break
+				end
+			end
+			r = sqrt(f1 * f1 + g1 * g1)
+			cs = f1 / r
+			sn = g1 / r
+			for i in 1:count
+				r *= safmn2
+			end
+		else
+			r = sqrt(f1 * f1 + g1 * g1)
+			cs = f1 / r
+			sn = g1 / r
+		end
+		if abs(f) > abs(g) && cs < 0
+			cs = -cs
+			sn = -sn
+			r = -r
+		end
+	end
+	return cs, sn, r
+end
 
 end
